9730a1e4ddba26e7e2856d6769179ba3171000e5
OkkashaAlly <steamnature006@gmail.com>
2023-08-29 17:18:24 +0300

    12 - Data Transformation

--- app/RQsuperheros/page.tsx
+++ app/RQsuperheros/page.tsx
@@ -8,33 +8,36 @@ const RQSuperheros = () => {
     console.log("Performing onSuccess handler, data", data);
   };
 
-   const onError = (data: any) => {
+  const onError = (data: any) => {
     console.log("Performing onError handler, data", data);
   };
-  
+
   const getSuperHeros = async () =>
     axios
-      .get("http://localhost:3004/superheros1")
+      .get("http://localhost:3004/superheros")
       .then(res => res.data as [{ id: string; name: string }]);
 
   type Heros = [{ id: string; name: string }];
 
-  const { data,refetch, isInitialLoading, isError, error } = useQuery<Heros, Error>(
-    ["super-heros"],
-    getSuperHeros, {
-      // cacheTime: 5000, // 5 seconds caches the data
-      // staleTime: 10000, // 10 seconds waits 10 seconds before refetching
-      // refetchOnMount: true, // refetch when the component mounts
-      // refetchOnWindowFocus: true, // refetch when the window is in focus
-      // refetchInterval: 2000, // refetch every 2 seconds on window focus
-      // refetchIntervalInBackground: true, // refetch in the background every 2 seconds
-      enabled: false, // if false, the query will not execute
-      onSuccess, // call back function after success of the query
-      onError, // call back function after error of the query
-    }
-  );
+  type HerosNames = string[];
 
-  if (isInitialLoading ) return <p>Loading...</p>;
+  const { data, refetch, isInitialLoading, isError, error } = useQuery<
+    any,
+    Error
+  >(["super-heros"], getSuperHeros, {
+    // cacheTime: 5000, // 5 seconds caches the data
+    // staleTime: 10000, // 10 seconds waits 10 seconds before refetching
+    // refetchOnMount: true, // refetch when the component mounts
+    // refetchOnWindowFocus: true, // refetch when the window is in focus
+    // refetchInterval: 2000, // refetch every 2 seconds on window focus
+    // refetchIntervalInBackground: true, // refetch in the background every 2 seconds
+    // enabled: false, // if false, the query will not execute
+    // onSuccess, // call back function after success of the query
+    // onError, // call back function after error of the query
+    select: (data: Heros) => data.map(item => item.name), // select the data you want to return
+  });
+
+  if (isInitialLoading) return <p>Loading...</p>;
 
   if (isError) return <p>Error: {error.message}</p>;
 
@@ -43,8 +46,8 @@ const RQSuperheros = () => {
       <div>RQSuperheros</div>
       <button onClick={() => refetch()}>Get superheros</button>
       <ul className="pl-8 pt-3 list-disc">
-        {data?.map(item => (
-          <li key={item.name}>{item.name}</li>
+        {data?.map((item: string) => (
+          <li key={item}>{item}</li>
         ))}
       </ul>
     </>
