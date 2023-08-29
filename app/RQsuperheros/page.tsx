061e7c74f04e82c9885b5a9118fe8b14da9553e8
OkkashaAlly <steamnature006@gmail.com>
2023-08-29 15:44:50 +0300

    10 - useQuery on click

--- app/RQsuperheros/page.tsx
+++ app/RQsuperheros/page.tsx
@@ -11,25 +11,27 @@ const RQSuperheros = () => {
 
   type Heros = [{ id: string; name: string }];
 
-  const { data, isLoading, isError, error } = useQuery<Heros, Error>(
+  const { data,refetch, isLoading, isInitialLoading, isError, error } = useQuery<Heros, Error>(
     ["super-heros"],
     getSuperHeros, {
       // cacheTime: 5000, // 5 seconds caches the data
       // staleTime: 10000, // 10 seconds waits 10 seconds before refetching
-      // refetchOnMount: true, // refetches when the component mounts
-      // refetchOnWindowFocus: true, // refetches when the window is in focus
-      // refetchInterval: 2000, // refetches every 2 seconds on window focus
-      // refetchIntervalInBackground: true, // refetches in the background every 2 seconds
+      // refetchOnMount: true, // refetch when the component mounts
+      // refetchOnWindowFocus: true, // refetch when the window is in focus
+      // refetchInterval: 2000, // refetch every 2 seconds on window focus
+      // refetchIntervalInBackground: true, // refetch in the background every 2 seconds
+      enabled: false, // if false, the query will not execute
     }
   );
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isInitialLoading ) return <p>Loading...</p>;
 
   if (isError) return <p>Error: {error.message}</p>;
 
   return (
     <>
       <div>RQSuperheros</div>
+      <button onClick={() => refetch()}>Get superheros</button>
       <ul className="pl-8 pt-3 list-disc">
         {data?.map(item => (
           <li key={item.name}>{item.name}</li>
