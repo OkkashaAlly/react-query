42cc32108ead3cccf3aa2f1841fd3c0b516fe309
OkkashaAlly <steamnature006@gmail.com>
2023-08-30 06:19:38 +0300

    13 - Custom Query Hook

--- app/RQsuperheros/page.tsx
+++ app/RQsuperheros/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useSuperHerosData } from "@/hooks/useSuperHerosData";
 
 const RQSuperheros = () => {
   const onSuccess = (data: any) => {
@@ -12,30 +11,10 @@ const RQSuperheros = () => {
     console.log("Performing onError handler, data", data);
   };
 
-  const getSuperHeros = async () =>
-    axios
-      .get("http://localhost:3004/superheros")
-      .then(res => res.data as [{ id: string; name: string }]);
-
-  type Heros = [{ id: string; name: string }];
-
-  type HerosNames = string[];
-
-  const { data, refetch, isInitialLoading, isError, error } = useQuery<
-    any,
-    Error
-  >(["super-heros"], getSuperHeros, {
-    // cacheTime: 5000, // 5 seconds caches the data
-    // staleTime: 10000, // 10 seconds waits 10 seconds before refetching
-    // refetchOnMount: true, // refetch when the component mounts
-    // refetchOnWindowFocus: true, // refetch when the window is in focus
-    // refetchInterval: 2000, // refetch every 2 seconds on window focus
-    // refetchIntervalInBackground: true, // refetch in the background every 2 seconds
-    // enabled: false, // if false, the query will not execute
-    // onSuccess, // call back function after success of the query
-    // onError, // call back function after error of the query
-    select: (data: Heros) => data.map(item => item.name), // select the data you want to return
-  });
+  const { data, refetch, isInitialLoading, isError, error } = useSuperHerosData(
+    onSuccess,
+    onError
+  );
 
   if (isInitialLoading) return <p>Loading...</p>;
 
---
+++ hooks/useSuperHerosData.ts
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const getSuperHeros = async () =>
+  axios
+    .get("http://localhost:3004/superheros")
+    .then(res => res.data as [{ id: string; name: string }]);
+
+type Heros = [{ id: string; name: string }];
+
+type Function = (data: Heros | Error) => void;
+
+export const useSuperHerosData = (onSuccess: Function, onError:Function) => {
+  return useQuery<any, Error>(["super-heros"], getSuperHeros, {
+    // cacheTime: 5000, // 5 seconds caches the data
+    // staleTime: 10000, // 10 seconds waits 10 seconds before refetching
+    // refetchOnMount: true, // refetch when the component mounts
+    // refetchOnWindowFocus: true, // refetch when the window is in focus
+    // refetchInterval: 2000, // refetch every 2 seconds on window focus
+    // refetchIntervalInBackground: true, // refetch in the background every 2 seconds
+    enabled: false, // if false, the query will not execute
+    // onSuccess, // call back function after success of the query
+    // onError, // call back function after error of the query
+    select: (data: Heros) => data.map(item => item.name), // select the data you want to return
+  });
+};
